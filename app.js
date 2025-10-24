f8ebc6ae951cef858bdf56e6b6f5e4d8baa66e92
Akash <akashpoddar492@gmail.com>
2025-10-24 12:54:27 +0530

    add edit, update, and delete routes for listings

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
     const  app =  express();
     const mongoose = require("mongoose");
     const Listing = require("./models/listing");
-    const path = require("path");   
+    const path = require("path");  
+    const methodOverride = require("method-override"); 
+    const ejsMate = require("ejs-mate");
 
     const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,6 +22,8 @@
     app.set("view engine", "ejs");
     app.set("views", path.join(__dirname , "views"));
     app.use(express.urlencoded({extended:true}));
+    app.use(methodOverride("_method"));
+    app.engine("ejs",ejsMate);
 
     app.get("/", (req,res) =>{
         res.send("I am root");
@@ -54,24 +58,31 @@
             //console.log(newListing)
         })
 
+        //Edit Route
+        app.get("/listings/:id/edit", async(req,res) =>{
+            let {id} = req.params;
+            const listing = await Listing.findById(id);
+            res.render("listings/edit.ejs", {listing});
+
+        });
+
+        //Update Route
+        app.put("/listings/:id" , async(req,res) =>{
+              let {id} = req.params;
+              await Listing.findByIdAndUpdate(id,{...req.body.listing} );
+              res.redirect(`/listings/${id}`);
+
+        });
+
+        //Delete Route
+        app.delete("/listings/:id" , async(req,res) =>{
+            let{id} = req.params;
+            await Listing.findByIdAndDelete(id);
+            res.redirect("/listings");
+        })
+
+
         
-
-    // app.get("/testListing" ,async (req,res)=>{
-    //     const sampleListing =new Listing({
-    //         title : "My new villa",
-    //         description : "By the beach",
-    //         image : "",
-    //         price : 1200,
-    //         location : "Calangute Goa",
-    //         country : "India",
-
-    //     });
-    //     await sampleListing.save();
-    //     console.log("sample was saved");
-    //     res.send("successfull test");
-    // })
-
-
     app.listen(8080,()=>{
         console.log("server is listing to port 8080");
     })
